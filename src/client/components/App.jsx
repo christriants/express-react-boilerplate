f70c71c7cd59a0d6d6494233a4e4b4a7d0a63f9f
christriants <30908909+christriants@users.noreply.github.com>
2018-09-21 17:03:33 +0300

    first react commit

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,10 +1,48 @@
-import React, { Component } from "react";
-import styles from "../scss/application.scss";
+/* eslint-disable */
+import React, { Component } from 'react';
+import styles from '../scss/application.scss';
 
 class App extends Component {
-  render() {
-    return <main className="App" />;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      liveStatus: false,
+    };
   }
+
+  componentWillMount() {
+    fetch('/live')
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        console.log(data.liveStatus);
+        this.setState({
+          liveStatus : data.liveStatus
+        })
+      });
+  }
+
+  render() {
+    const { liveStatus } = this.state;
+    const videoSrc = liveStatus ? '' : 'guitar-solo.mp4';
+
+    return (
+      <main className="App" >
+        <h1>Chris' RTMP Player</h1>
+        <video id="player" className="video-js" controls preload="auto" poster="livestream.jpg" data-setup="{}">
+          <source id='video' src={videoSrc} type='video/mp4' />
+          <p className="vjs-no-js">
+            <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+          </p>
+        </video>
+      </main>
+    );
+
+
+  }
+
 }
 
 export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,75 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
+const crypto = require('crypto');
 
+// Initialize our server
 const app = express();
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, "./../../")));
-
+app.use(express.static(path.join(__dirname, './../../')));
 const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
+  console.log(`Server started on port ${port}`);
 });
+
+// Values for the Livestream API
+const API_SECRET = 'HTuRddLgDjCZcnJdo0kvpYY9HOX9RCFA';
+const clientId = 8143;
+const timestamp = (new Date()).getTime();
+const apiToken = generateToken('readonly', timestamp);
+
+// Setup our routes
+app.get('/live', (request, response) => {
+  const isLive = getLivestream(apiToken);
+  const liveResponse = {
+    liveStatus: isLive,
+  };
+  // getLivestream()
+  //   .then(data => {
+
+  //   })
+  //   .catch(error => {
+  //     response.json({ error: 'We hit an error from livestream api'});
+  //   });
+  // const liveResponse = { value: 'hey chris' };
+  response.json(liveResponse);
+});
+
+// Calls livestream API to get event status
+async function getLivestream(livestreamToken) {
+  const getReq = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const livestreamEvent = `https://livestreamapis.com/v3/accounts/12963240/events/5037587?clientId=${clientId}&token=${livestreamToken}&timestamp=${timestamp}`;
+  const res = await fetch(livestreamEvent, getReq);
+  console.log(res.json());
+  const json = res.json();
+  const { isLive } = json;
+  // console.log(isLive);
+  return isLive;
+}
+
+// Calls livestream API
+// const source = async function changeVideo() {
+//   const isEventLive = await getLivestream();
+//   if (isEventLive === true) {
+//     // window.document.getElementById('video').src = livestream;
+//     const livestream = `https://livestreamapis.com/v3/accounts/12963240/events/5037587/live.m3u8?clientId=${clientId}&token=${apiToken}&timestamp=${timestamp}`;
+//     console.log(livestream);
+//   } else {
+//     // document.getElementById('video').src = '';
+//     console.log('guitar-solo.mp4');
+//   }
+// };
+
+// source();
+
+// Helpers
+function generateToken(scope) {
+  const hmac = crypto.createHmac('md5', API_SECRET);
+  const tokenString = `${API_SECRET}:${scope}:${timestamp}`;
+  hmac.update(tokenString);
+  const crypted = hmac.digest('hex');
+  return crypted;
+}
